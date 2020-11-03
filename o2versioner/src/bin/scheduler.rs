74dc981ddbccaadd9666594b406decd10545d67e
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-11-02 20:37:16 -0500

    Added logging support

--- o2versioner/src/bin/scheduler.rs
+++ o2versioner/src/bin/scheduler.rs
@@ -1,8 +1,20 @@
 use actix_web::{web, App, HttpServer};
+use env_logger;
+use log::info;
 use o2versioner::scheduler::*;
+
+fn init_logger() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.target(env_logger::Target::Stdout);
+    builder.filter_level(log::LevelFilter::Debug);
+    builder.init();
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    init_logger();
+    info!("Hello from scheduler exe");
+
     HttpServer::new(|| App::new().route("/", web::get().to(appserver_handler::greet)))
         .bind("127.0.0.1:8080")?
         .run()
--- o2versioner/src/scheduler/appserver_handler.rs
+++ o2versioner/src/scheduler/appserver_handler.rs
@@ -1,5 +1,7 @@
 use actix_web::{HttpRequest, Responder};
+use log::info;
 
 pub async fn greet(req: HttpRequest) -> impl Responder {
+    info!("Received request from {}", req.peer_addr().unwrap());
     format!("Hello {:?}!", req)
 }
