a2a4926090e2e169c0d2779f325a50788e683a4d
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-11-22 16:43:35 -0500

    Implemented util/config

--- o2versioner/src/util/config.rs
+++ o2versioner/src/util/config.rs
@@ -1,18 +1,55 @@
 #![allow(dead_code)]
-use serde::{Deserialize, Serialize};
+use config;
+use serde::Deserialize;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Eq, PartialEq, Deserialize, Clone)]
 pub struct Config {
-    sequencer: SequencerConfig,
-    dbproxy: Vec<DbProxyConfig>,
+    pub sequencer: SequencerConfig,
+    pub dbproxy: Vec<DbProxyConfig>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+impl Config {
+    pub fn from_file(path: &str) -> Config {
+        let mut source = config::Config::default();
+        source.merge(config::File::with_name(path)).unwrap();
+        source.try_into().expect("Invalid Configuration format!")
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Deserialize, Clone)]
 pub struct SequencerConfig {
-    addr: String,
+    pub addr: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Eq, PartialEq, Deserialize, Clone)]
 pub struct DbProxyConfig {
-    addr: String,
+    pub addr: String,
+}
+
+/// Unit test for `Config`
+#[cfg(test)]
+mod tests_config {
+    use super::{Config, DbProxyConfig, SequencerConfig};
+
+    #[test]
+    fn test_from_file() {
+        let conf = Config::from_file("tests/config/conf1.toml");
+
+        assert_eq!(
+            conf,
+            Config {
+                sequencer: SequencerConfig {
+                    addr: String::from("127.0.0.1:9876")
+                },
+                dbproxy: vec![
+                    DbProxyConfig {
+                        addr: String::from("127.0.0.1:8876")
+                    },
+                    DbProxyConfig {
+                        addr: String::from("127.0.0.1:8877")
+                    }
+                ]
+            }
+        );
+    }
 }
