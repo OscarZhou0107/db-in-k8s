2ce08869c41282d069ca74119bb4571429bae9e2
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-11-22 16:03:21 -0500

    Ignored warnings from core and dispatcher

--- o2versioner/src/dbproxy/core.rs
+++ o2versioner/src/dbproxy/core.rs
@@ -1,59 +1,56 @@
+#![allow(warnings)]
+use crate::core::sql::Operation as OperationType;
 use crate::core::version_number::{TableVN, TxVN, VN};
-use crate::core::sql::{Operation as OperationType};
-use std::{collections::HashMap};
 use mysql_async::prelude::*;
+use std::collections::HashMap;
 use tokio::runtime::Runtime;
 pub struct DbVersion {
-    table_versions : HashMap<String, u64>
+    table_versions: HashMap<String, u64>,
 }
 
 impl DbVersion {
-    pub fn new(table_versions : HashMap<String, u64>) -> DbVersion{
-        DbVersion {table_versions : table_versions }
+    pub fn new(table_versions: HashMap<String, u64>) -> DbVersion {
+        DbVersion {
+            table_versions: table_versions,
+        }
     }
 
-    pub fn release_on_transaction(&mut self, transaction_version : TxVN) {
-        transaction_version.table_vns
-        .iter()
-        .for_each(|t|{
-            match self.table_versions.get_mut(&t.table) {
+    pub fn release_on_transaction(&mut self, transaction_version: TxVN) {
+        transaction_version
+            .table_vns
+            .iter()
+            .for_each(|t| match self.table_versions.get_mut(&t.table) {
                 Some(v) => *v = t.vn + 1,
-                None => println!("Table {} not found to release version.",t.table),
-            }       
+                None => println!("Table {} not found to release version.", t.table),
+            })
+    }
+
+    pub fn release_on_tables(&mut self, tables: Vec<TableVN>) {
+        tables.iter().for_each(|t| match self.table_versions.get_mut(&t.table) {
+            Some(v) => *v = t.vn + 1,
+            None => println!("Table {} not found to release version.", t.table),
         })
     }
 
-    pub fn release_on_tables(&mut self, tables : Vec<TableVN>) {
-        tables
-        .iter()
-        .for_each(|t|{
-            match self.table_versions.get_mut(&t.table) {
-                Some(v) => *v = t.vn + 1,
-                None => println!("Table {} not found to release version.",t.table),
-            }       
-        })
-    }
-
-    pub fn violate_version(& self, transaction_version : Operation) -> bool {
-        transaction_version.table_vns
-        .iter()
-        .any(|t|{
+    pub fn violate_version(&self, transaction_version: Operation) -> bool {
+        transaction_version.table_vns.iter().any(|t| {
             if let Some(v) = self.table_versions.get(&t.table) {
-                return *v < t.vn; 
+                return *v < t.vn;
             } else {
                 return true;
-            }})
+            }
+        })
     }
 }
 
 pub struct Repository {
-    conn : mysql_async::Conn
+    conn: mysql_async::Conn,
 }
 
 impl Repository {
-    pub async fn new(pool : mysql_async::Pool) -> Repository{
+    pub async fn new(pool: mysql_async::Pool) -> Repository {
         let conn = pool.get_conn().await.unwrap();
-        Repository {conn : conn}
+        Repository { conn: conn }
     }
 
     pub async fn start_transaction(&mut self) {
@@ -61,12 +58,20 @@ impl Repository {
     }
 
     pub async fn execute_read(&mut self) -> QueryResult {
-        let _ = self.conn.query_iter("INSERT INTO cats (name, owner, birth) VALUES ('haha2', 'haha3', CURDATE())").await.unwrap();
+        let _ = self
+            .conn
+            .query_iter("INSERT INTO cats (name, owner, birth) VALUES ('haha2', 'haha3', CURDATE())")
+            .await
+            .unwrap();
         test_helper_get_query_result_non_release()
     }
 
-    pub async fn execute_write(&mut self) -> QueryResult { 
-        let _ = self.conn.query_iter("INSERT INTO cats (name, owner, birth) VALUES ('haha2', 'haha3', CURDATE())").await.unwrap();
+    pub async fn execute_write(&mut self) -> QueryResult {
+        let _ = self
+            .conn
+            .query_iter("INSERT INTO cats (name, owner, birth) VALUES ('haha2', 'haha3', CURDATE())")
+            .await
+            .unwrap();
         test_helper_get_query_result_non_release()
     }
 
@@ -79,20 +84,19 @@ impl Repository {
         self.conn.query_drop("ROLLBACK;").await.unwrap();
         test_helper_get_query_result_version_release()
     }
-
 }
 
 #[derive(Default)]
 pub struct QueryResult {
-    pub result : String,
-    pub version_release : bool,
-    pub contained_newer_versions : Vec<TableVN>
+    pub result: String,
+    pub version_release: bool,
+    pub contained_newer_versions: Vec<TableVN>,
 }
 
 #[derive(Clone)]
 pub struct Operation {
-    pub transaction_id : String,
-    pub task : Task,
+    pub transaction_id: String,
+    pub task: Task,
     pub table_vns: Vec<TableVN>,
 }
 
@@ -104,34 +108,72 @@ pub enum Task {
     COMMIT,
 }
 //================================Test================================//
-fn test_helper_get_query_result_version_release() -> QueryResult{
+fn test_helper_get_query_result_version_release() -> QueryResult {
     let mock_table_vs = vec![
-        TableVN {table : "table1".to_string(), vn : 0, op : OperationType::R},
-        TableVN {table : "table2".to_string(), vn : 0, op : OperationType::R},
-        ];
-    QueryResult {result : " ".to_string(), version_release : true, contained_newer_versions : mock_table_vs}
+        TableVN {
+            table: "table1".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+        TableVN {
+            table: "table2".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+    ];
+    QueryResult {
+        result: " ".to_string(),
+        version_release: true,
+        contained_newer_versions: mock_table_vs,
+    }
 }
 
-fn test_helper_get_query_result_non_release() -> QueryResult{
+fn test_helper_get_query_result_non_release() -> QueryResult {
     let mock_table_vs = vec![
-        TableVN {table : "table1".to_string(), vn : 0, op : OperationType::R},
-        TableVN {table : "table2".to_string(), vn : 0, op : OperationType::R},
-        ];
-    QueryResult {result : " ".to_string(), version_release : false, contained_newer_versions : mock_table_vs}
+        TableVN {
+            table: "table1".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+        TableVN {
+            table: "table2".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+    ];
+    QueryResult {
+        result: " ".to_string(),
+        version_release: false,
+        contained_newer_versions: mock_table_vs,
+    }
 }
 
 #[test]
 fn voilate_dbversion_should_return_true() {
-    //Prepare 
+    //Prepare
     let mut table_versions = HashMap::new();
     table_versions.insert("table1".to_string(), 0);
     table_versions.insert("table2".to_string(), 0);
-    let db_version = DbVersion {table_versions : table_versions};
+    let db_version = DbVersion {
+        table_versions: table_versions,
+    };
     let versions = vec![
-        TableVN {table : "table1".to_string(), vn : 0, op : OperationType::R},
-        TableVN {table : "table2".to_string(), vn : 1, op : OperationType::R},
-        ];
-    let operation = Operation { table_vns : versions, transaction_id : "t1".to_string() , task : Task::READ};
+        TableVN {
+            table: "table1".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+        TableVN {
+            table: "table2".to_string(),
+            vn: 1,
+            op: OperationType::R,
+        },
+    ];
+    let operation = Operation {
+        table_vns: versions,
+        transaction_id: "t1".to_string(),
+        task: Task::READ,
+    };
     //Action
     //Assert
     assert!(db_version.violate_version(operation));
@@ -139,16 +181,30 @@ fn voilate_dbversion_should_return_true() {
 
 #[test]
 fn obey_dbversion_should_return_false() {
-    //Prepare 
+    //Prepare
     let mut table_versions = HashMap::new();
     table_versions.insert("table1".to_string(), 0);
     table_versions.insert("table2".to_string(), 0);
-    let db_version = DbVersion {table_versions : table_versions};
+    let db_version = DbVersion {
+        table_versions: table_versions,
+    };
     let versions = vec![
-        TableVN {table : "table1".to_string(), vn : 0, op : OperationType::R},
-        TableVN {table : "table2".to_string(), vn : 0, op : OperationType::R},
-        ];
-    let operation = Operation { table_vns : versions, transaction_id : "t1".to_string() , task : Task::READ};
+        TableVN {
+            table: "table1".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+        TableVN {
+            table: "table2".to_string(),
+            vn: 0,
+            op: OperationType::R,
+        },
+    ];
+    let operation = Operation {
+        table_vns: versions,
+        transaction_id: "t1".to_string(),
+        task: Task::READ,
+    };
     //Action
     //Assert
     assert!(!db_version.violate_version(operation));
@@ -156,39 +212,36 @@ fn obey_dbversion_should_return_false() {
 
 #[test]
 fn test_sql_connection() {
-
     let mut rt = Runtime::new().unwrap();
-    
-    rt.block_on(async  {
+
+    rt.block_on(async {
         let url = "mysql://root:Rayh8768@localhost:3306/test";
         let pool = mysql_async::Pool::new(url);
-    
         match pool.get_conn().await {
-            Ok(_) => {println!{"OK"}},
-            Err(e) => {println!("error is =============================== : {}",e)}
+            Ok(_) => {
+                println! {"OK"}
+            }
+            Err(e) => {
+                println!("error is =============================== : {}", e)
+            }
         };
     })
-
 }
 
 #[test]
 fn run_sql_query() {
-
     let mut rt = Runtime::new().unwrap();
-    
-    rt.block_on(async  {
+
+    rt.block_on(async {
         let url = "mysql://root:Rayh8768@localhost:3306/test";
         let pool = mysql_async::Pool::new(url);
         //let mut wtr = csv::Writer::from_writer(io::stdout());
-    
-        let mut conn = pool.get_conn().await.unwrap(); 
+
+        let mut conn = pool.get_conn().await.unwrap();
 
         let mut raw = conn.query_iter("select * from cats").await.unwrap();
-        let results: Vec<mysql_async::Row>= raw.collect().await.unwrap();
-        results
-        .iter()
-        .for_each(|r| {
-
+        let results: Vec<mysql_async::Row> = raw.collect().await.unwrap();
+        results.iter().for_each(|r| {
             println!("len {}", r.len());
             // r
             // .unwrap()
@@ -198,6 +251,5 @@ fn run_sql_query() {
         })
         //let se = tokio_serde::Serializer<Vec<mysql_async::Row>> serialize(results);
         //let de : tokio_serde::Deserializer<Vec<mysql_async::Row>> = tokio_serde::Deserializer();
-        
     });
 }
--- o2versioner/src/dbproxy/dispatcher.rs
+++ o2versioner/src/dbproxy/dispatcher.rs
@@ -1,97 +1,103 @@
-use tokio::sync::mpsc;
-use tokio::sync::Semaphore;
-use std::{sync::Mutex, collections::HashMap};
+#![allow(warnings)]
+use super::core::{DbVersion, Operation, QueryResult, Repository, Task};
 use std::sync::Arc;
+use std::{collections::HashMap, sync::Mutex};
+use tokio::sync::mpsc;
 use tokio::sync::Notify;
-use super::core::{QueryResult, DbVersion, Task, Operation, Repository};
+use tokio::sync::Semaphore;
 
 pub struct Dispatcher {}
 
 impl Dispatcher {
-    
-    pub fn run(mut pending_queue : Arc<Mutex<Vec<Operation>>>,
-            sender : mpsc::Sender<QueryResult>,
-            sql_url : String,
-            mut version: Arc<Mutex<DbVersion>>,
-            semaphor : Arc<Semaphore>,
-            mut task_notify : Arc<Notify>,
-            mut version_notify : Arc<Notify>,
-            transactions : Arc<Mutex<HashMap<String, mpsc::Sender<Operation>>>>,
+    pub fn run(
+        mut pending_queue: Arc<Mutex<Vec<Operation>>>,
+        sender: mpsc::Sender<QueryResult>,
+        sql_url: String,
+        mut version: Arc<Mutex<DbVersion>>,
+        semaphor: Arc<Semaphore>,
+        mut task_notify: Arc<Notify>,
+        mut version_notify: Arc<Notify>,
+        transactions: Arc<Mutex<HashMap<String, mpsc::Sender<Operation>>>>,
     ) {
-        tokio::spawn( async move {
+        tokio::spawn(async move {
             let pool = mysql_async::Pool::new(sql_url);
-    
             loop {
-        
                 wait_for_new_task_or_version_release(&mut task_notify, &mut version_notify).await;
                 let operations = get_all_version_ready_task(&mut pending_queue, &mut version);
-               
-                operations
-                .iter()
-                .for_each(|op| {
+
+                operations.iter().for_each(|op| {
                     let op = op.clone();
                     let mut lock = transactions.lock().unwrap();
                     if !lock.contains_key(&op.transaction_id) {
-    
-                            let (mut ts, mut tr) : (mpsc::Sender<Operation>, mpsc::Receiver<Operation>)=  mpsc::channel(1);
-            
-                            lock.insert(op.transaction_id.clone(), ts);
-                            let pool_clone = pool.clone();
-                            let semaphor_clone = semaphor.clone();
-                            let mut sender_clone = sender.clone();
-                            
-                            tokio::spawn(async move {
-                                let mut result : QueryResult = Default::default();
-                                {  
-                                    semaphor_clone.acquire().await;
+                        let (mut ts, mut tr): (mpsc::Sender<Operation>, mpsc::Receiver<Operation>) = mpsc::channel(1);
 
-                                    let mut repo = Repository::new(pool_clone).await;
-                                    repo.start_transaction().await;
+                        lock.insert(op.transaction_id.clone(), ts);
+                        let pool_clone = pool.clone();
+                        let semaphor_clone = semaphor.clone();
+                        let mut sender_clone = sender.clone();
 
-                                    while let Some(operation) = tr.recv().await { 
-                                        match  operation.task {
-                                            Task::READ => {result = repo.execute_read().await;},
-                                            Task::WRITE => {result = repo.execute_write().await;},
-                                            Task::COMMIT => {result = repo.commit().await; break},
-                                            Task::ABORT => {result = repo.abort().await; break},
+                        tokio::spawn(async move {
+                            let mut result: QueryResult = Default::default();
+                            {
+                                semaphor_clone.acquire().await;
+
+                                let mut repo = Repository::new(pool_clone).await;
+                                repo.start_transaction().await;
+
+                                while let Some(operation) = tr.recv().await {
+                                    match operation.task {
+                                        Task::READ => {
+                                            result = repo.execute_read().await;
                                         }
-                                        sender_clone.send(result).await;
+                                        Task::WRITE => {
+                                            result = repo.execute_write().await;
+                                        }
+                                        Task::COMMIT => {
+                                            result = repo.commit().await;
+                                            break;
+                                        }
+                                        Task::ABORT => {
+                                            result = repo.abort().await;
+                                            break;
+                                        }
                                     }
+                                    sender_clone.send(result).await;
                                 }
-                                //sender_clone.send(result).await;
-                            });
+                            }
+                            //sender_clone.send(result).await;
+                        });
                     }
                 });
-                
-                operations
-                .iter()
-                .for_each( |op| {
+
+                operations.iter().for_each(|op| {
                     let op = op.clone();
                     let mut sender;
                     {
                         sender = transactions.lock().unwrap().get(&op.transaction_id).unwrap().clone();
                     }
-                    tokio::spawn(async move { 
+                    tokio::spawn(async move {
                         sender.send(op).await;
                     });
                 });
             }
         });
     }
-
 }
 
-async fn wait_for_new_task_or_version_release(new_task_notify : &mut Arc<Notify>, version_notify : &mut Arc<Notify>){
+async fn wait_for_new_task_or_version_release(new_task_notify: &mut Arc<Notify>, version_notify: &mut Arc<Notify>) {
     let n1 = new_task_notify.notified();
     let n2 = version_notify.notified();
 
-    tokio::select! { 
-        _ = n1 => {}
-        _ = n2 => {}
-     };
+    tokio::select! {
+       _ = n1 => {}
+       _ = n2 => {}
+    };
 }
 
-fn get_all_version_ready_task(pending_queue : &mut Arc<Mutex<Vec<Operation>>>, version : &mut Arc<Mutex<DbVersion>>) -> Vec<Operation> {
+fn get_all_version_ready_task(
+    pending_queue: &mut Arc<Mutex<Vec<Operation>>>,
+    version: &mut Arc<Mutex<DbVersion>>,
+) -> Vec<Operation> {
     let mut lock = pending_queue.lock().unwrap();
 
     //Unstable feature
@@ -102,18 +108,14 @@ fn get_all_version_ready_task(pending_queue : &mut Arc<Mutex<Vec<Operation>>>, v
     // .collect()
 
     let ready_ops = lock
-    .split(|op| {
-        version.lock().unwrap().violate_version(op.clone())
-    })
-    .fold(Vec::new(),| mut acc_ops , ops|  {
-        ops.iter().for_each(|op| {
-            acc_ops.push(op.clone());
+        .split(|op| version.lock().unwrap().violate_version(op.clone()))
+        .fold(Vec::new(), |mut acc_ops, ops| {
+            ops.iter().for_each(|op| {
+                acc_ops.push(op.clone());
+            });
+            acc_ops
         });
-        acc_ops
-    });
 
-    lock.retain(|op| 
-        version.lock().unwrap().violate_version(op.clone())
-    );
+    lock.retain(|op| version.lock().unwrap().violate_version(op.clone()));
     ready_ops
 }
