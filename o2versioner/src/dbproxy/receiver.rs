88225cf18bf8e6a759b71bce67cf0f984f9d8807
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-11-23 19:28:38 -0500

    clean up warnings in receiver

--- o2versioner/src/dbproxy/receiver.rs
+++ o2versioner/src/dbproxy/receiver.rs
@@ -1,37 +1,35 @@
-use tokio::{net::tcp::OwnedReadHalf, sync::mpsc};
-use tokio::stream::StreamExt;
-use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
-use tokio_serde::{formats::SymmetricalJson, SymmetricallyFramed};
-use std::{sync::Mutex};
-use std::sync::Arc;
-use tokio::sync::Notify;
-use crate::comm::scheduler_dbproxy::Message;
 use super::core::Operation;
+use crate::comm::scheduler_dbproxy::Message;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::net::tcp::OwnedReadHalf;
+use tokio::stream::StreamExt;
+use tokio::sync::Notify;
+use tokio_serde::{formats::SymmetricalJson, SymmetricallyFramed};
+use tokio_util::codec::{FramedRead, LengthDelimitedCodec};
 
 pub struct Receiver {}
 
 // Box<SymmetricallyFramed<FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>,Message,SymmetricalJson<Message>>>
 impl Receiver {
-    pub fn run(pending_queue: Arc<Mutex<Vec<Operation>>>,
-            notify : Arc<Notify>,
-            tcp_read : OwnedReadHalf,
-        ) {
-          
+    pub fn run(pending_queue: Arc<Mutex<Vec<Operation>>>, notify: Arc<Notify>, tcp_read: OwnedReadHalf) {
         let mut deserializer = SymmetricallyFramed::new(
             FramedRead::new(tcp_read, LengthDelimitedCodec::new()),
             SymmetricalJson::<Message>::default(),
-            );
+        );
 
-        tokio::spawn( async move {
+        tokio::spawn(async move {
             while let Some(msg) = deserializer.try_next().await.unwrap() {
                 match msg {
                     Message::SqlRequest(op) => {
                         pending_queue.lock().unwrap().push(op);
                         notify.notify();
-                        }
-                    other => {println!("nope")}
+                    }
+                    _other => {
+                        println!("nope")
+                    }
                 }
             }
-        });  
+        });
     }
 }
