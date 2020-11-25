eb35dc607a58bc749856c30f743c48e1408982d5
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-11-24 23:37:35 -0500

    Refactored scheduler to use start_tcplistener

--- o2versioner/src/scheduler/handler.rs
+++ o2versioner/src/scheduler/handler.rs
@@ -1,11 +1,12 @@
 //use crate::comm::scheduler_sequencer;
 use crate::core::sql::{SqlRawString, TxTable};
 use crate::core::version_number::TxVN;
+use crate::util::tcp;
 use futures::prelude::*;
 use log::info;
 use std::net::SocketAddr;
+use tokio::net::TcpStream;
 use tokio::net::ToSocketAddrs;
-use tokio::net::{TcpListener, TcpStream};
 use tokio_serde::formats::SymmetricalJson;
 use tokio_serde::SymmetricallyFramed;
 use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
@@ -26,30 +27,17 @@ use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 /// `|`  - Or;
 /// `[]` - Optional
 ///
-pub async fn main<A: ToSocketAddrs>(addr: A, max_connection: Option<usize>) {
-    let mut listener = TcpListener::bind(addr).await.unwrap();
-
-    let mut cur_num_connection = 0;
-    let mut spawned_tasks = Vec::new();
-    loop {
-        let (tcp_stream, peer_addr) = listener.accept().await.unwrap();
-
-        info!("Connection established with [{}] {}", cur_num_connection, peer_addr);
-        cur_num_connection += 1;
-
-        // Spawn a new thread for each tcp connection
-        spawned_tasks.push(tokio::spawn(process_connection(tcp_stream)));
-
-        // An optional max number of connections allowed
-        if let Some(nmax) = max_connection {
-            if cur_num_connection >= nmax {
-                break;
-            }
-        }
-    }
-
-    // Wait on all spawned tasks to finish
-    futures::future::join_all(spawned_tasks).await;
+pub async fn main<A>(addr: A, max_connection: Option<usize>)
+where
+    A: ToSocketAddrs + std::fmt::Debug + Clone,
+{
+    tcp::start_tcplistener(
+        addr,
+        |tcp_stream| process_connection(tcp_stream),
+        max_connection,
+        Some("Scheduler"),
+    )
+    .await;
 }
 
 /// Process the `tcp_stream` for a single connection
