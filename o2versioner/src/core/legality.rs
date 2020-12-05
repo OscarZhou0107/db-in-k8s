a6286cf9bd71866cc005afa7462157888fb2ae75
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-12-05 16:02:01 -0500

    support early release in scheduler

--- o2versioner/src/core/legality.rs
+++ o2versioner/src/core/legality.rs
@@ -11,11 +11,11 @@ pub enum Legality {
     Panic(&'static str),
 }
 
-/// Use this function as a single point for query diagnostics at the
-/// very beginning stage of request handling, so that the later stages can simply
-/// panic
+/// Use this function as a single point for query diagnostics,
+/// all legalization must be done before this function call.
+/// Code after this function call in later stages can simply panic.
 impl Legality {
-    pub fn check(msql: &Msql, txvn_opt: &Option<TxVN>) -> Self {
+    pub fn final_check(msql: &Msql, txvn_opt: &Option<TxVN>) -> Self {
         match msql {
             Msql::BeginTx(_begintx) => {
                 if txvn_opt.is_some() {
@@ -25,17 +25,29 @@ impl Legality {
                 }
             }
             Msql::Query(query) => {
-                let txvn = txvn_opt.as_ref().unwrap();
-                if query.tableops().access_pattern() == AccessPattern::Mixed {
-                    Self::Critical("Does not support query with mixed R and W")
-                } else if txvn.get_from_tableops(&query.tableops()).is_err() {
-                    Self::Critical("Query is using tables not declared in the BeginTx")
-                } else if txvn.get_from_ertables(&query.early_release_tables()).is_err() {
-                    Self::Critical(
-                        "Tables marked for early release was not declared in the BeginTx or has already been released",
-                    )
+                if let Some(txvn) = txvn_opt.as_ref() {
+                    if query.tableops().access_pattern() == AccessPattern::Mixed {
+                        Self::Critical("Does not support query with mixed R and W")
+                    } else if txvn.get_from_tableops(&query.tableops()).is_err() {
+                        Self::Critical("Query is using tables not declared in the BeginTx")
+                    } else if txvn.get_from_ertables(&query.early_release_tables()).is_err() {
+                        Self::Critical(
+                            "Tables marked for early release was not declared in the BeginTx or has already been released",
+                        )
+                    } else {
+                        Self::Legal
+                    }
                 } else {
-                    Self::Legal
+                    if query.tableops().access_pattern() == AccessPattern::ReadOnly {
+                        if query.has_early_release() {
+                            // TODO: change it to critical after supporting fast single R query
+                            Self::Panic("Does not support early release release for fast single R query")
+                        } else {
+                            Self::Panic("Does not support fast single R query yet")
+                        }
+                    } else {
+                        Self::Panic("Query does not have a valid BeginTx")
+                    }
                 }
             }
             Msql::EndTx(_endtx) => {
