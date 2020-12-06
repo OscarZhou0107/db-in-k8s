14df18b04018868720530ed9a915a5135637d680
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-12-06 18:18:37 -0500

    update single_run.py

--- logging_analyzer/single_run.py
+++ logging_analyzer/single_run.py
@@ -1,8 +1,9 @@
 import csv
 from datetime import datetime, timedelta
+import argparse
 import itertools
 try:
-    from dateutil import parser
+    from dateutil import parser as dateutil_parser
 except:
     print('Error:', 'pip install python-dateutil')
 
@@ -19,13 +20,13 @@ def parse_csv(log_path):
 def prepare_db(db):
     def convert_db(db):
         for row in db:
-            row['initial_timestamp'] = parser.isoparse(row['initial_timestamp'])
-            row['final_timestamp'] = parser.isoparse(row['final_timestamp'])
+            row['initial_timestamp'] = dateutil_parser.isoparse(row['initial_timestamp'])
+            row['final_timestamp'] = dateutil_parser.isoparse(row['final_timestamp'])
 
     def append_latency_db(db):
         for row in db:
             row['latency'] = row['final_timestamp'] - row['initial_timestamp']
-    
+
     convert_db(db)
     append_latency_db(db)
 
@@ -34,7 +35,7 @@ def print_row(row):
     modified_row = row.copy()
     modified_row['initial_timestamp'] = modified_row['initial_timestamp'].isoformat()
     modified_row['final_timestamp'] = modified_row['final_timestamp'].isoformat()
-    print('Info:', *modified_row.items())
+    print('Info:', *map(lambda kv: (str(kv[0]), str(kv[1])), modified_row.items()))
 
 
 def group_by_time_interval_since_beginning(db, filter_func=None, interval_length=timedelta(seconds=1)):
@@ -50,24 +51,24 @@ def group_by_time_interval_since_beginning(db, filter_func=None, interval_length
     '''
     if filter_func is not None:
         db = list(filter(filter_func, db))
-    
+
     if len(db) == 0:
         return
-    
+
     # Base is set to be the earliest initial_timestamp
     first_timestamp = min(db, key=lambda row: row['initial_timestamp'])['initial_timestamp']
     print('Info:', 'first_timestamp:', first_timestamp)
-    
+
     # Sort all rows by final_timestamp
     db = sorted(db, key=lambda row: row['final_timestamp'])
     grouped_by_sec = itertools.groupby(db, key=lambda row: int((row['final_timestamp']-first_timestamp)/interval_length))
-    
+
     groups_of_rows = list()
     secs = list()
     for sec, rows in grouped_by_sec:
         groups_of_rows.append(sorted(rows, key=lambda row: row['final_timestamp']))
         secs.append(sec)
-    
+
     return list(zip(secs, groups_of_rows))
 
 
@@ -82,10 +83,15 @@ def get_throughput(grouped_by_time_interval_since_beginning):
     return [(len_after_first_group, num_finished_within_the_interval)]
     '''
     return list(map(lambda id_rows: (id_rows[0], len(id_rows[1])), grouped_by_time_interval_since_beginning))
-    
 
-def main():
-    db = parse_csv('o2versioner/logging/perf_201206_072034.csv')
+
+def init(parser):
+    parser.add_argument('--log_file', type=str, required=True, help='log file')
+
+
+def main(args):
+    print('Info:', 'Analyzing', args.log_file)
+    db = parse_csv(args.log_file)
     prepare_db(db)
 
     # [(len_after_first_group, rows)]
@@ -106,6 +112,7 @@ def main():
         print('Info:', item)
 
 
-
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    init(parser)
+    main(parser.parse_args())
