9698e6cb8f5b5a1d6ad394f643b65119bf3ad53c
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-12-06 03:29:54 -0500

    analyzer script

--- logging_analyzer/single_run.py
+++ logging_analyzer/single_run.py
@@ -1,10 +1,47 @@
 import csv
+from datetime import datetime
+try:
+    from dateutil import parser
+except:
+    print('pip install python-dateutil')
 
-def parse_file(log_path):
+
+def parse_csv(log_path):
+    '''
+    [{key:value}]
+    '''
     with open(log_path) as csvfile:
         csvreader = csv.DictReader(csvfile)
-        for line in csvreader:
-            print(line)
+        return list(csvreader)
 
 
-parse_file('o2versioner/logging/perf_201206_072034.csv')
+def prepare_db(db):
+    def convert_db(db):
+        for row in db:
+            row['initial_timestamp'] = parser.isoparse(row['initial_timestamp'])
+            row['final_timestamp'] = parser.isoparse(row['final_timestamp'])
+
+    def append_latency_db(db):
+        for row in db:
+            row['latency'] = row['final_timestamp'] - row['initial_timestamp']
+    
+    convert_db(db)
+    append_latency_db(db)
+
+
+def calculate_throughput(db):
+    '''
+    [num_request_completed@sec0, num_request_completed@sec1, num_request_completed@sec2]
+    '''
+    pass
+
+def main():
+    db = parse_csv('o2versioner/logging/perf_201206_072034.csv')
+    prepare_db(db)
+
+    for row in db:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
