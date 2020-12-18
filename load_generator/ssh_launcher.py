f99da52a0145470fad6487b143c60c14dfa7adc2
Sining <qsnsidney@gmail.com>
2020-12-18 00:57:43 -0500

    spread clients over all machines

--- load_generator/ssh_launcher.py
+++ load_generator/ssh_launcher.py
@@ -5,6 +5,7 @@ import multiprocessing
 import argparse
 import math
 import signal
+import random
 
 DEBUG = 0
 
@@ -36,7 +37,8 @@ if __name__ == "__main__":
     if args.mock_db:
         mock_db = "--mock_db"
 
-    host_abbr = ["206", "207", "208", "209", "210"]
+    host_abbr = ['132', '133', '134', '135', '136', '137', '138', '139', '140', '141', '142', '143', '144', '145', '146', '147', '148', '149', '150', '151', '152', '153', '154', '155', '156', '157', '158', '159', '160', '161', '162', '163', '164', '165', '166', '167', '168', '169', '170', '171', '172', '173', '174', '175', '176', '177', '178', '179', '180', '201', '202', '203', '204', '205', '206', '207', '208', '209', '210', '211', '212', '213', '214', '215', '216', '217', '218', '219', '220', '221', '222', '223', '224', '225', '226', '227', '228', '229', '230', '231', '232', '233', '234', '235', '236', '237', '238', '239', '240', '241', '242', '243', '244', '245', '246', '247', '248', '249']
+    random.shuffle(host_abbr)
     hosts = ["ug" + x + ".eecg.utoronto.ca" for x in host_abbr]
     print("host: {}".format(hosts[0]))
     host_num = len(hosts)
@@ -44,10 +46,14 @@ if __name__ == "__main__":
 
     client_range_per_host = []
     num = 0
-    for i in range(host_num - 1):
+    for i in range(host_num):
         num = num + client_num_per_host
+        if num >= client_num:
+            client_range_per_host.append("{} {}".format(num - client_num_per_host, client_num))
+            break
         client_range_per_host.append("{} {}".format(num - client_num_per_host, num))
-    client_range_per_host.append("{} {}".format(num, client_num))
+
+    host_num = len(client_num_per_host)
 
     #host_index = [str(x) for x in range(host_num)]
     
