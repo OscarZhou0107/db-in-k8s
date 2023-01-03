4bce221cfd172b7e4d38d1c772716261cfc9bc43
Weizhou Wang <wangweizhou0221@gmail.com>
2023-01-02 23:10:03 -0500

    Removed uncessary infinite loop

--- dbproxy_start.sh
+++ dbproxy_start.sh
@@ -4,10 +4,8 @@ REPLICA_ID=${HOSTNAME: -1}
 echo "Starting DBProxy $REPLICA_ID..."
 if [[ $REPLICA_ID -gt '1' ]]
 then
-    cargo run 0 --dbproxy -c ./o2versioner/replicates$[REPLICA_ID-2].toml &
-    sleep 3
-    { echo block; echo connect $[REPLICA_ID-2]; echo unblock; echo break; } | netcat scheduler-deployment-0.scheduler-service.default.svc.cluster.local 9999
-    while true; do sleep 1; done
+    { sleep 10; echo block; echo connect $[REPLICA_ID-2]; echo unblock; echo break; } | netcat scheduler-deployment-0.scheduler-service.default.svc.cluster.local 9999 &
+    cargo run 0 --dbproxy -c ./o2versioner/replicates$[REPLICA_ID-2].toml 
 else
     cargo run 0 --dbproxy -c ./o2versioner/conf_dbproxy$REPLICA_ID.toml
 fi
