ae35a109039cead6409cebb548a9883d834f08cf
Weizhou Wang <wangweizhou0221@gmail.com>
2023-01-20 00:59:26 -0500

    Mount perf to local volume

--- k8s_start.sh
+++ k8s_start.sh
@@ -3,12 +3,13 @@ read -p "Please wait for some time to initialize the dashboard... (Press any key
 echo ===================================Delete Old Deployment==========================================
 kubectl delete -f k8s/deployment
 sleep 10 # Wait for old deployment fully deleted
-# docker container prune
+docker container prune
 echo ===================================Starting Dbproxies=============================================
 kubectl apply -f k8s/deployment/dbproxy0-deployment.yaml
 sleep 5
 echo ===================================Starting Scheduler_sequencer===================================
-kubectl apply -f k8s/deployment/scheduler-deployment.yaml
+cat k8s/deployment/scheduler-deployment.yaml | sed s+{{path}}+$(pwd)+g > k8s/deployment/scheduler-deployment_tmp.yaml
+kubectl apply -f k8s/deployment/scheduler-deployment_cur.yaml
 sleep 5 # Need time for system to establish connection
 echo ===================================System Started=================================================
 kubectl get pods # Display the running pods
