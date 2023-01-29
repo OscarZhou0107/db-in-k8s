1f224ed38c997f78e5c627427c3d806f307beb5d
Weizhou Wang <wangweizhou0221@gmail.com>
2023-01-29 15:59:28 -0500

    testing

--- test_document.sh
+++ test_document.sh
@@ -2,13 +2,13 @@
 
 rm test_log.txt
 # 每次选一个replica的数量
-for replicas in 1 2 4 8
+for replicas in 8
 do 
     # 选client的数量
     for clients in 500
     do
         # 选workload
-        for mix in 1 2 3
+        for mix in 1
         do 
             # kill上一次的process
             pgrep "o2" | xargs kill
@@ -23,7 +23,7 @@ do
                 cargo run $proxy --dbproxy &
                 sleep 2
             done
-            cargo run -- --scheduler & 
+            cargo run -- --scheduler 
             sleep 3
             # 看一下一共起来的replica数量和设置的一不一样
             if [ $(ps ux | grep -c o2) -ne $((replicas + 3)) ]
@@ -33,9 +33,9 @@ do
                 exit
             fi
             sleep 5
-            python3 ./load_generator/launcher.py --port 2077 --mix $mix --range 0 $clients --ip 127.0.0.1 --mock_db --path ./load_generator
+            # python3 ./load_generator/launcher.py --port 2077 --mix $mix --range 0 $clients --ip 127.0.0.1 --mock_db --path ./load_generator
             sleep 1
-            { echo perf; echo break; } | netcat localhost 9999
+            # { echo perf; echo break; } | netcat localhost 9999
         done
     done
 done
