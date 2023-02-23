aec335992f3dc97189abab28703c0c0b0be7c754
Weizhou Wang <wangweizhou0221@gmail.com>
2023-02-23 17:00:46 -0500

    Start TPC-W on a separate machine

--- k8s_start.sh
+++ k8s_start.sh
@@ -13,10 +13,11 @@ kubectl apply -f k8s/deployment/scheduler-deployment_tmp.yaml
 sleep 5 # Need time for system to establish connection
 echo ===================================System Started=================================================
 kubectl get pods # Display the running pods
-echo ===================================Start TPC-W====================================================
-read -p "Press any key to start TPC-W..." key
-kubectl apply -f k8s/deployment/load-generator-deployment.yaml
-echo Running TPC-W...
+echo Please go to another machine under the same WI-FI network and execute: \"python3 ./load_generator/launcher.py --port 32077 --mix 3 --range 0 200 --ip \<your_local_IP\> --mock_db --path ./load_generator\"
+# echo ===================================Start TPC-W====================================================
+# read -p "Press any key to start TPC-W..." key
+# kubectl apply -f k8s/deployment/load-generator-deployment.yaml
+# echo Running TPC-W...
 # kubectl top pods
 
 # sleep 10
@@ -25,3 +26,4 @@ echo Running TPC-W...
 # sleep 5
 # kubectl scale statefulsets dbproxy0-deployment --replicas=4
 # kubectl exec scheduler-deployment-0 -- bash -c "netcat -e new_dbproxy_start.sh scheduler-deployment-0.scheduler-service.default.svc.cluster.local 9999"
+
