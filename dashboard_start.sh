a8f5d5e21747e7e26f12f1955c5efeff910c3d47
Weizhou Wang <wangweizhou0221@gmail.com>
2023-03-24 17:42:59 -0400

    Added scripts to better starting system

--- dashboard_start.sh
+++ dashboard_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo ===================================Delete Old Deployment==========================================
-kubectl -n kubernetes-dashboard delete serviceaccount admin-user
-kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user
-pkill kubectl proxy
-kubectl delete -f k8s/k8s_dashboard
-echo ===================================Deploy Metrics Server==========================================
-kubectl apply -f k8s/k8s_dashboard/metrics-server.yaml
+# echo ===================================Delete Old Deployment==========================================
+# kubectl -n kubernetes-dashboard delete serviceaccount admin-user
+# kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user
+# pkill kubectl proxy
+# kubectl delete -f k8s/k8s_dashboard
+# echo ===================================Deploy Metrics Server==========================================
+# kubectl apply -f k8s/k8s_dashboard/metrics-server.yaml
 echo ===================================Deploy the Dashboard UI========================================
 kubectl apply -f k8s/k8s_dashboard/dashboard.yaml
 echo Starting the Dashboard Server...
